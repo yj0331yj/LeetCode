e5bf22a516a13bf6eafa208f9137ca41217131bc
JeongYoonjin <166682949+yj0331yj@users.noreply.github.com>
2024-08-21 12:00:26 +0900

    Time: 41 ms (64.20%), Space: 11.9 MB (99.07%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -1,16 +1,27 @@
+from collections import deque
+
 class Solution(object):
     def canVisitAllRooms(self, rooms):
         n = len(rooms)
         visited = [False] * n
-
-        def dfs(cur_v, rooms, visited):
-            visited[cur_v] = True
-            for next_v in rooms[cur_v]:
-                if not visited[next_v]:
-                    dfs(next_v, rooms, visited)
-        dfs(0, rooms, visited)
-
+        
+        def bfs(start_v):
+            q = deque()
+            q.append(start_v)
+            visited[start_v] = True
+            
+            while q:
+                cur_v = q.popleft()
+                for next_v in rooms[cur_v]:
+                    if not visited[next_v]:
+                        q.append(next_v)
+                        visited[next_v] = True
+                        
+        bfs(0)
+        
         for v in visited:
             if not v:
                 return False
-        return True
+        # all함수 -> visited에 하나라도 False가 있으면 False를 return, 전체가 다 True일 때만 True를 return
+        return all(visited)
+                
