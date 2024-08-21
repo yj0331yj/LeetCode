c5ffa51ff12c6b32fedbca33d5da28a733748205
JeongYoonjin <166682949+yj0331yj@users.noreply.github.com>
2024-08-21 10:26:38 +0900

    Time: 30 ms (96.67%), Space: 12.5 MB (41.82%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,8 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        num_dict = {}
+        memo = {}
         for i, num in enumerate(nums):
             n = target - num
-            if n in num_dict: 
-                return [num_dict[n], i]  
-            num_dict[num] = i
+            if n in memo:
+                return [memo[n], i]
+            memo[num] = i
